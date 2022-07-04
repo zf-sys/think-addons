6fae9933b1267a3bba966abe0f1c624673d6c1ec
zifeng <287851074@qq.com>
2022-07-04 17:16:08 +0800

    fix🐛:  修改module的值

--- src/helper.php
+++ src/helper.php
@@ -30,13 +30,15 @@ Route::group('addons', function () {
     if ($pathinfo[0] == 'addons' && isset($pathinfo[2])) {
         // 获取路由地址
         $route = explode('.', $pathinfo[1]); 
-        $module = array_shift($route);
-        $className = ucfirst(array_pop($route));
+        $module = $pathinfo[0];
+        $className = array_shift($route);
+        $className2 = ucfirst(array_pop($route));
         array_push($route, $className);
-        $controller = join('\\', $route);
+        array_push($route, $className2);
+        $controller = $route[1];
         $type = array_shift($route);
         // 生成view_path
-        $view_path = Env::get('addons_path') . $module . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR;
+        $view_path = Env::get('addons_path') . $className . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR;
         Config::set('template.view_path', $view_path);
         // 中间件
         $middleware = [];
@@ -45,15 +47,16 @@ Route::group('addons', function () {
             $middleware = (array)$config[$type];
         }
         // 请求转入
-        Route::rule(':rule', "\\addons\\{$module}\\controller\\{$controller}@{$pathinfo[2]}")
+        Route::rule(':rule', "\\addons\\{$className}\\controller\\{$controller}@{$pathinfo[2]}")
             ->middleware($middleware);
     }
 })->middleware(function ($request, \Closure $next) {
     // 路由地址
     $pathinfo = explode('/', $request->path());
     $rules = explode('.', $pathinfo[1]);
-    $request->setModule(array_shift($rules));
-    $request->setController(join('/', $rules));
+    $request->setModule($pathinfo[0]);
+    $request->setController($rules[1]);
+    // $request->setController(join('/', $rules));
     $request->setAction($pathinfo[2]);
     $_name = $pathinfo[2];
     unset($request->$_name);
@@ -70,6 +73,7 @@ Route::group('addons', function () {
             $request->$_name  = $vo;
         }
     }
+    $request->plugin_name = $rules[0];
     $module = $request->module();
     $_file2 = './addons/'.$module.'/config/function.php';
     if(is_dir('./addons/'.$module) && file_exists($_file2) ){
